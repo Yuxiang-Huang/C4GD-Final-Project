1aa99df5cf566fbdaeed3123d2c715ff47d0cc61
EitanTreiger <eitant@outlook.com>
2022-07-18 09:12:12 -0700

    Update PlayerControll.cs

--- Chessward/Assets/Scripts/PlayerControll.cs
+++ Chessward/Assets/Scripts/PlayerControll.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerControll : MonoBehaviour
 {
+    public float jumpForce = 10.0f;
+    private Rigidbody playerRb;
+    public bool isOnGround = true;
+    public float gravityModifier;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
