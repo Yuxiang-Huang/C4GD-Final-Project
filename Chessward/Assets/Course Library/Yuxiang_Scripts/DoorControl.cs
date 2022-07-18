2124c421b1a3b5b25c64df7c3c46905f5d3e6f80
Yuxiang Huang <yhuang30@stuy.edu>
2022-07-18 12:53:16 -0400

    Added a button

--- Chessward/Assets/Course Library/Yuxiang_Scripts/DoorControl.cs
+++ Chessward/Assets/Course Library/Yuxiang_Scripts/DoorControl.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorControl : MonoBehaviour
 {
+    bool doorOpen;
+    public Button open;
+    public Button close;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorOpen = false;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
