34aa753d3c4fc2ae6f81b051ef0b012f9489b771
EitanTreiger <eitant@outlook.com>
2022-07-18 11:35:05 -0700

    rotating view

--- Chessward/Assets/Scripts/Camera.cs
+++ Chessward/Assets/Scripts/Camera.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public GameObject Player;
-    private Vector3 offset = new Vector3(0, 2.2f, 0.1f);
+    private Vector3 offset = new Vector3(0, 2.5f, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = Player.transform.position + offset;
+        transform.rotation = Player.transform.rotation;
     }
 }
