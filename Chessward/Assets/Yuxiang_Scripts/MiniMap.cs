2f32c4674a3c38caff2c8997c8b5b9c08c2f7bc9
Yuxiang Huang <yhuang30@stuy.edu>
2022-07-18 23:58:11 -0400

    Minimap working already?

---
+++ Chessward/Assets/Yuxiang_Scripts/MiniMap.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject image;
+    RectTransform size;
+
+    public float xSpace;
+    public float xStart;
+    public float ySpace;
+    public float yStart;
+
+    public int x;
+    public int y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        RectTransform size = gameObject.GetComponent<RectTransform>();
+        xStart = (float) (transform.position.x - size.rect.width / 8 * 3.5);
+        yStart = (float) (transform.position.y - size.rect.height / 8 * 3.5);
+
+        xSpace = size.rect.width / 8;
+        ySpace = size.rect.height / 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = (int) player.transform.position.x / 100;
+        y = (int)player.transform.position.z / 100;
+        image.transform.position = new Vector3(xStart + x * xSpace, yStart + y * ySpace, 0);
+    }
+}
