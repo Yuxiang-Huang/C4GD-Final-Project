52bd9c8cd21257f5f26e39475f46a91df6846c7d
Yuxiang Huang <yhuang30@stuy.edu>
2022-07-18 20:23:33 -0400

    Able to make a room with code

--- Chessward/Assets/Yuxiang_Scripts/MapBuilder.cs
+++ Chessward/Assets/Yuxiang_Scripts/MapBuilder.cs
@@ -20,21 +20,34 @@ public class MapBuilder : MonoBehaviour
 
     void CreateWhiteRoom(float x, float z)
     {
+        Vector3 center = new Vector3(x + roomLength / 2, 0, z + roomLength / 2);
+
         //floor
-        Instantiate(WhiteFloor, new Vector3(x + roomLength / 2, 0, z + roomLength / 2),
-            WhiteFloor.transform.rotation);
+        Instantiate(WhiteFloor, center, WhiteFloor.transform.rotation);
 
         //front
         GameObject front = Instantiate(WhiteWall,
-            new Vector3(x + roomLength / 2, roomLength / 2, z + roomLength),
+            new Vector3(center.x, roomLength / 2, center.z + roomLength / 2),
             WhiteFloor.transform.rotation);
         front.transform.Rotate(-90, 0, 0);
 
-        //left
-        GameObject left = Instantiate(WhiteWall,
-            new Vector3(x + roomLength / 2, roomLength / 2, z + roomLength),
+        ////back
+        GameObject back = Instantiate(WhiteWall,
+            new Vector3(center.x, roomLength / 2, center.z - roomLength / 2),
             WhiteFloor.transform.rotation);
-        left.transform.Rotate(0, 0, 90);
+        back.transform.Rotate(90, 0, 0);
+
+        ////left
+        GameObject left = Instantiate(WhiteWall,
+            new Vector3(center.x - roomLength / 2, roomLength / 2, center.z),
+            WhiteFloor.transform.rotation);
+        left.transform.Rotate(0, 0, -90);
+
+        ////right
+        GameObject right = Instantiate(WhiteWall,
+            new Vector3(center.x + roomLength / 2, roomLength / 2, center.z),
+            WhiteFloor.transform.rotation);
+        right.transform.Rotate(0, 0, 90);
 
     }
 }
