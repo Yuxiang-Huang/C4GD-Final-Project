e768667656c601983b7e52feaa75fdcd3eb19bed
Yuxiang Huang <yhuang30@stuy.edu>
2022-07-18 20:49:20 -0400

    Made a white room with doors using code

--- Chessward/Assets/Yuxiang_Scripts/MapBuilder.cs
+++ Chessward/Assets/Yuxiang_Scripts/MapBuilder.cs
@@ -9,14 +9,53 @@ public class MapBuilder : MonoBehaviour
     public GameObject WhiteFloor;
     public GameObject WhiteWall;
     public GameObject WhiteWallWDoor;
+    public GameObject BlackFloor;
+    public GameObject BlackWall;
+    public GameObject BlackWallWDoor;
 
     private float roomLength = 100;
+
+    private float yForDoors = 69.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         CreateWhiteRoom(0, 0);
     }
+
+    void CreateBlackRoomWithFourDoors(float x, float z)
+    {
+        Vector3 center = new Vector3(x + roomLength / 2, 0, z + roomLength / 2);
+
+        //floor
+        Instantiate(BlackFloor, center, BlackFloor.transform.rotation);
+
+        //front
+        GameObject front = Instantiate(BlackWallWDoor,
+            new Vector3(center.x, yForDoors, center.z + roomLength / 2),
+            BlackFloor.transform.rotation);
+
+        ////back
+        //GameObject back = Instantiate(BlackWallWDoor,
+        //    new Vector3(center.x, yForDoors, center.z - roomLength / 2),
+        //    BlackFloor.transform.rotation);
+        //back.transform.Rotate(0, 180, 0);
+
+        ////left
+        //GameObject left = Instantiate(BlackWallWDoor,
+        //    new Vector3(center.x - roomLength / 2, yForDoors, center.z),
+        //    BlackFloor.transform.rotation);
+        //left.transform.Rotate(0, -90, 0);
+
+        ////right
+        //GameObject right = Instantiate(BlackWallWDoor,
+        //    new Vector3(center.x + roomLength / 2, yForDoors, center.z),
+        //    BlackFloor.transform.rotation);
+        //right.transform.Rotate(0, 90, 0);
+    }
+
+
+
 
     void CreateWhiteRoom(float x, float z)
     {
@@ -48,6 +87,39 @@ public class MapBuilder : MonoBehaviour
             new Vector3(center.x + roomLength / 2, roomLength / 2, center.z),
             WhiteFloor.transform.rotation);
         right.transform.Rotate(0, 0, 90);
-
     }
+
+    void CreateBlackRoom(float x, float z)
+    {
+        Vector3 center = new Vector3(x + roomLength / 2, 0, z + roomLength / 2);
+
+        //floor
+        Instantiate(BlackFloor, center, BlackFloor.transform.rotation);
+
+        //front
+        GameObject front = Instantiate(BlackWall,
+            new Vector3(center.x, roomLength / 2, center.z + roomLength / 2),
+            BlackFloor.transform.rotation);
+        front.transform.Rotate(-90, 0, 0);
+
+        ////back
+        GameObject back = Instantiate(BlackWall,
+            new Vector3(center.x, roomLength / 2, center.z - roomLength / 2),
+            BlackFloor.transform.rotation);
+        back.transform.Rotate(90, 0, 0);
+
+        ////left
+        GameObject left = Instantiate(BlackWall,
+            new Vector3(center.x - roomLength / 2, roomLength / 2, center.z),
+            BlackFloor.transform.rotation);
+        left.transform.Rotate(0, 0, -90);
+
+        ////right
+        GameObject right = Instantiate(BlackWall,
+            new Vector3(center.x + roomLength / 2, roomLength / 2, center.z),
+            BlackFloor.transform.rotation);
+        right.transform.Rotate(0, 0, 90);
+    }
+
+
 }
