bc7d5a867542addcdf4ee52c6ba158fe89a65e83
EitanTreiger <eitant@outlook.com>
2022-07-19 07:22:17 -0700

    Reload

--- Chessward/Assets/Scripts/Guny.cs
+++ Chessward/Assets/Scripts/Guny.cs
@@ -8,10 +8,12 @@ public class Guny : MonoBehaviour
     private Vector3 offset = new Vector3(0, 15, 0);
     public GameObject projectilePrefab;
     private float shootTime;
+    private int magSize;
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.Find("Player");
+        magSize = 30;
     }
     // Update is called once per frame
     void LateUpdate()
@@ -21,11 +23,20 @@ public class Guny : MonoBehaviour
         transform.rotation = Player.transform.rotation;
         transform.position += transform.right;
         transform.position += transform.forward;
-        if (Input.GetKey(KeyCode.Q) && shootTime > 0.2)
+        if (Input.GetKey(KeyCode.Q) && shootTime > 0.2 && magSize > 0)
         {
             Vector3 offset2 = transform.up * 0.5f + transform.forward * 6f;
             shootTime = 0;
             Instantiate(projectilePrefab, transform.position + offset2, transform.rotation);
+            magSize = magSize - 1;
+            StartCoroutine(MagCheck());
+        }
+    }
+    IEnumerator MagCheck() {
+        if (magSize <= 0)
+        {
+            yield return new WaitForSeconds(3);
+            magSize = 30;
         }
     }
 }
