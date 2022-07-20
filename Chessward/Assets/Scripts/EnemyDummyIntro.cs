187b3bcec7f279b62480fc3c53503b7eeffd9fed
Yuxiang Huang <yhuang30@stuy.edu>
2022-07-20 13:02:19 -0400

    Combine EnemyDummy and BlockTeleport script

--- Chessward/Assets/Scripts/EnemyDummyIntro.cs
+++ Chessward/Assets/Scripts/EnemyDummyIntro.cs
@@ -10,25 +10,43 @@ public class EnemyDummyIntro : MonoBehaviour
     public GameObject EnemyWeapon;
     public SpawnEnemy spawnEnemyScript;
 
+    public TeleportManager teleportManager;
+    public int xPos;
+    public int yPos;
+    public float roomLength = 100;
+    public string pieceName;
+
     // Start is called before the first frame update
     void Start()
     {
+        Player = GameObject.Find("Player");
         health = 100;
         damage = 10;
         EnemyWeapon.gameObject.SetActive(true);
         spawnEnemyScript = GameObject.Find("SpawnManager").GetComponent<SpawnEnemy>();
+        teleportManager = GameObject.Find("Teleport Manager").GetComponent<TeleportManager>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Player = GameObject.Find("Player");
-
+    { 
         Vector3 targetPostition = new Vector3(Player.transform.position.x,
                                         this.transform.position.y,
                                         Player.transform.position.z);
         this.transform.LookAt(targetPostition);
+
+        int xPosNow = (int)(transform.position.x / roomLength);
+        int yPosNow = (int)(transform.position.z / roomLength);
+
+        if (xPosNow != xPos || yPosNow != yPos)
+        {
+            teleportManager.enemySquare[xPos][yPos] = false;
+            xPos = xPosNow;
+            yPos = yPosNow;
+            teleportManager.enemySquare[xPos][yPos] = true;
+        }
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("bullet"))
@@ -40,6 +58,7 @@ public class EnemyDummyIntro : MonoBehaviour
             Destroy(gameObject);
             EnemyWeapon.gameObject.SetActive(false);
             spawnEnemyScript.numOfEnemies--;
+            teleportManager.enemySquare[xPos][yPos] = false;
         }
     }
 
