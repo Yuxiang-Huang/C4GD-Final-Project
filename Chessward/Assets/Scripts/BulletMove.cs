08d9ff9f924e8fa0b4908d20611afab7c73134a0
Yuxiang Huang <yhuang30@stuy.edu>
2022-07-20 13:22:58 -0400

    Player bullet

--- Chessward/Assets/Scripts/BulletMove.cs
+++ Chessward/Assets/Scripts/BulletMove.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class BulletMove : MonoBehaviour
 {
     private float bSpeed = 80.0f;
-    public GameObject Player;
-    private float maxDist = 80.0f;
+    //public GameObject Player;
+    //private float maxDist = 80.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         transform.Rotate(new Vector3(90, 0, 0));
-        Player = GameObject.Find("Player");
+        //Player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.up * Time.deltaTime * bSpeed);
-        float dist = (transform.position - Player.transform.position).magnitude;
-        if (dist > maxDist)
+        //float dist = (transform.position - Player.transform.position).magnitude;
+        //if (dist > maxDist)
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground") || other.CompareTag("Wall"))
         {
             Destroy(gameObject);
         }
